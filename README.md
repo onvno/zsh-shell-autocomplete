507852612f8365bd40e3a139c7d60b1c97e08579
onvno <leewei2020@126.com>
2018-08-11 16:34:31 +0800

    autoc plugin

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # zsh-shell-autocomplete
-A Simple ZSH Plugin, AutoComplete File Path
+A Simple ZSH Plugin, AutoComplete File Path, like Vim Plugin `ctrlp`
+
+
+
+#### 基本说明
+
+* 类似vim直接在编辑器中快速查找文件打开文件，如`ctrlp`
+* 因vscode编辑器自带终端，能够快速切换打开shell，遂根据需求，基于`incr-0.2`完成此插件。无需在vscode侧边栏滚来滚去查找\打开文件。
+
+
+
+#### 支持范围
+
+* Linux && MacOS
+* VSCode
+
+
+
+#### 功能演示
+
+![](./screen.gif)
+
+
+
+#### 使用说明
+
+1. 命令行使用`code`快速开启vscode:
+
+   ```
+   $ vi ~/.zshrc
+   
+   > # vscode使用命令行打开
+   > export PATH=/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin:$PATH
+   
+   # 保存退出
+   $ source ~/.zshrc
+   
+   # 命令行使用code打开当前目录
+   $ code .
+   ```
+
+2. 拷贝zsh插件`autoc.zsh`
+
+   ```
+   # 建议拷贝至oh-my-zsh个人插件目录:~/.oh-my-zsh/custom/plugins
+   $ mkdir autoc
+   $ cp your/path/autoc.zsh autoc/autoc.zsh
+   
+   # 添加插件source
+   $ vi ~/.zshrc
+   > source ~/.oh-my-zsh/custom/plugins/autoc/autoc*.zsh
+   
+   # 配置生效
+   $ souce ~/.zshrc
+   ```
+
+   
+
+####  参考
+
+* [incr-0.2](http://mimosa-pudica.net/zsh-incremental.html)
+
+
+
+#### TODO
+
+- 可以支持sublime & webstorm快速打开
---
+++ autoc.zsh
@@ -0,0 +1,113 @@
+# Incremental completion for zsh
+# by y.fujii <y-fujii at mimosa-pudica.net>, public domain
+
+
+autoload -U compinit
+zle -N self-insert self-insert-incr
+zle -N backward-delete-char-incr
+compinit
+
+bindkey -M emacs '^h' backward-delete-char-incr
+bindkey -M emacs '^?' backward-delete-char-incr
+
+unsetopt automenu
+compdef -d scp
+compdef -d tar
+compdef -d make
+compdef -d java
+compdef -d svn
+compdef -d cvs
+
+# TODO:
+#     cp dir/
+
+now_predict=0
+
+function limit-completion
+{
+	if ((compstate[nmatches] <= 1)); then
+		zle -M ""
+	elif ((compstate[list_lines] > 50)); then
+		compstate[list]=""
+		zle -M "too many matches."
+	fi
+}
+
+function correct-prediction
+{
+	if ((now_predict == 1)); then
+		if [[ "$BUFFER" != "$buffer_prd" ]] || ((CURSOR != cursor_org)); then
+			now_predict=0
+		fi
+	fi
+}
+
+function remove-prediction
+{
+	if ((now_predict == 1)); then
+		BUFFER="$buffer_org"
+		now_predict=0
+	fi
+}
+
+function show-prediction
+{
+	# assert(now_predict == 0)
+	if
+		((PENDING == 0)) &&
+		((CURSOR > 1)) &&
+		[[ "$PREBUFFER" == "" ]] &&
+		[[ "$BUFFER[CURSOR]" != " " ]]
+	then
+	
+		cursor_org="$CURSOR"
+		buffer_org="$BUFFER"
+		comppostfuncs=(limit-completion)
+		zle complete-word
+		cursor_prd="$CURSOR"
+		buffer_prd="$BUFFER"
+		if [[ "$buffer_org[1,cursor_org]" == "$buffer_prd[1,cursor_org]" ]]; then
+			CURSOR="$cursor_org"
+			if [[ "$buffer_org" != "$buffer_prd" ]] || ((cursor_org != cursor_prd)); then
+				now_predict=1
+			fi
+		else
+			BUFFER="$buffer_org"
+			CURSOR="$cursor_org"
+		fi
+		echo -n "\e[32m"
+	else
+		zle -M ""
+	fi
+}
+
+function preexec
+{
+	echo -n "\e[39m"
+}
+
+function self-insert-incr
+{
+	correct-prediction
+	remove-prediction
+
+	if zle .self-insert; then
+		if [ "${BUFFER:0:5}" == "code " ]; then
+			show-prediction
+		fi
+	fi
+}
+
+function backward-delete-char-incr
+{
+	correct-prediction
+	remove-prediction
+	if zle backward-delete-char; then
+		if (( CURSOR > 4 )); then
+			# echo "\n~~${BUFFER}~~${CURSOR}~~\n"
+			show-prediction
+		else
+			remove-prediction
+		fi
+	fi
+}
